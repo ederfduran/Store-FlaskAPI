a3771441038c795a1fd2eadaaab825912c24a426
Eder Duran <ederduran@Eders-MacBook-Pro.local>
2021-07-06 11:09:15 -0500

    add type hinting

--- models/store.py
+++ models/store.py
@@ -1,18 +1,22 @@
+from models.item import ItemJson
+from typing import Dict, List, Union
 from db import db
+from models.item import ItemJson
 
+StoreJson = Dict[str, Union[int, str, List[ItemJson]]]
 
 class StoreModel(db.Model):
     __tablename__ = "stores"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
+    name = db.Column(db.String(80), unique=True)
     # SQLAlchemy is smart enough to be able to construct a many-to-one relationship from this
     items = db.relationship("ItemModel", lazy="dynamic")
 
-    def __init__(self, name):
+    def __init__(self, name:str):
         self.name = name
 
-    def json(self):
+    def json(self) -> StoreJson:
         return {
             "id": self.id,
             "name": self.name,
@@ -23,17 +27,17 @@ class StoreModel(db.Model):
         }
 
     @classmethod
-    def find_by_name(cls, name)-> "StoreModel":
+    def find_by_name(cls, name:str)-> "StoreModel":
         return cls.query.filter_by(name=name).first()
 
     @classmethod
-    def find_all(cls):
+    def find_all(cls) -> List["StoreModel"]:
         return cls.query.all()
 
-    def save_to_db(self):
+    def save_to_db(self) -> None:
         db.session.add(self)
         db.session.commit()
 
-    def delete_from_db(self):
+    def delete_from_db(self) -> None:
         db.session.delete(self)
         db.session.commit()
--- resources/item.py
+++ resources/item.py
@@ -1,38 +1,35 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import (
-    jwt_required,
-    get_jwt,
-    get_jwt_identity
-)
+from flask_jwt_extended import jwt_required
 from models.item import ItemModel
 
+BLANK_ERROR = " '{}' This field cannot be left blank!"
+NAME_ALREADY_EXISTS = "An item with name '{}' already exists."
+ERROR_INSERTING = "An error occurred while inserting the item."
+ITEM_NOT_FOUND = "Item not found."
+ITEM_DELETED = "Item deleted."
 
 class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument(
-        "price", type=float, required=True, help="This field cannot be left blank!"
+        "price", type=float, required=True, help=BLANK_ERROR.format("price")
     )
     parser.add_argument(
-        "store_id", type=int, required=True, help="Every item needs a store_id."
+        "store_id", type=int, required=True, help=BLANK_ERROR.format("store_id")
     )
 
-    @jwt_required  
-    def get(self, name):
+    def get(self, name:str):
         item = ItemModel.find_by_name(name)
         if item:
             return item.json(), 200
-        return {"message": "Item not found."}, 404
+        return {"message": ITEM_NOT_FOUND}, 404
 
     # highly sensitive endpoint as it require a fresh JWT
     # cannot be a JWT generated via the Token Refresh functionality.
     # A fresh token is the most secure token since it means the user just authenticated
     @jwt_required(fresh=True)
-    def post(self, name):
+    def post(self, name:str):
         if ItemModel.find_by_name(name):
-            return (
-                {"message": "An item with name '{}' already exists.".format(name)},
-                400,
-            )
+            return {"message": NAME_ALREADY_EXISTS.format(name)}, 400
         # Retrieves method payload
         data = Item.parser.parse_args()
 
@@ -42,7 +39,7 @@ class Item(Resource):
             item.save_to_db()
         except Exception as ex:
             print(ex.args)
-            return {"message": "An error occurred while inserting the item."}, 500
+            return {"message": ERROR_INSERTING}, 500
 
         return item.json(), 201
 
@@ -50,18 +47,14 @@ class Item(Resource):
     #  Claims are arbitrary pieces of data that can be included 
     #  in the JWT when it is created
     @jwt_required
-    def delete(self, name):
-        claims = get_jwt()
-        if not claims["is_admin"]:
-            return {"message": "Admin privilege required."}, 401
-
+    def delete(self, name:str):
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
-            return {"message": "Item deleted."}, 200
-        return {"message": "Item not found."}, 404
+            return {"message": ITEM_DELETED}, 200
+        return {"message": ITEM_NOT_FOUND}, 404
 
-    def put(self, name):
+    def put(self, name:str):
         data = Item.parser.parse_args()
 
         item = ItemModel.find_by_name(name)
@@ -79,26 +72,7 @@ class Item(Resource):
 class ItemList(Resource):
     # optional can be very handy if you want loggedd-out users 
     # to be able to see some data, but allow logged-in users see more data
-    @jwt_required(optional=True)
     def get(self):
-        """
-        Here we get the JWT identity, and then if the user is logged in (we were able to get an identity)
-        we return the entire item list.
-
-        Otherwise we just return the item names.
-
-        This could be done with e.g. see orders that have been placed, but not see details about the orders
-        unless the user has logged in.
-        """
-        user_id = get_jwt_identity()
-        print(user_id)
         items = [item.json() for item in ItemModel.find_all()]
-        if user_id:
-            return {"items": items}, 200
-        return (
-            {
-                "items": [item["name"] for item in items],
-                "message": "More data available if you log in.",
-            },
-            200,
-        )
+        return {"items": items}, 200
+    
